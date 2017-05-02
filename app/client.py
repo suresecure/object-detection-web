b786bdfddc7f1d3a6b794a11d5cf4ddcffc6b950
xunchangqing <xunchangqing@qq.com>
2017-05-02 17:14:18 +0800

    test client enhance

--- app/client.py
+++ app/client.py
@@ -4,8 +4,10 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado.httpclient import HTTPRequest
 from tornado import gen, web
 import datetime
+import mimetypes
 # import requests
 import time
+import ast
 import grequests
 
 request_count = 0
@@ -24,38 +26,89 @@ def sleep_test():
 # for i in range(100):
     # sleep_test()
 
+def encode_multipart_formdata(fields, files):
+    """
+    fields is a sequence of (name, value) elements for regular form fields.
+    files is a sequence of (name, filename, value) elements for data to be
+    uploaded as files.
+    Return (content_type, body) ready for httplib.HTTP instance
+    """
+    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+    CRLF = '\r\n'
+    L = []
+    for (key, value) in fields:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"' % key)
+        L.append('')
+        L.append(value)
+    for (key, filename, value) in files:
+        filename = filename.encode("utf8")
+        L.append('--' + BOUNDARY)
+        L.append(
+            'Content-Disposition: form-data; name="%s"; filename="%s"' % (
+                key, filename
+            )
+        )
+        L.append('Content-Type: %s' % get_content_type(filename))
+        L.append('')
+        L.append(value)
+    L.append('--' + BOUNDARY + '--')
+    L.append('')
+    body = CRLF.join(L)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
+
+def get_content_type(filename):
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
 def handle_response(response):
     global response_count
     response_count += 1
     if response.error:
         print "Error:", response.error
+    res_body = ast.literal_eval(response.body)
+    if(res_body.has_key('error')):
+        print res_body
     else:
+        # pass
         print response_count, response.body
 http_client = AsyncHTTPClient(max_clients=2000)
-post_request = tornado.httpclient.HTTPRequest('http://localhost:8090/person_detection', method='POST', body="xyz")
+
+with open('/home/mythxcq/helmet.jpg', 'rb') as f:
+    image_content = f.read()
+content_type, body = encode_multipart_formdata([], [('image', 'helmet.jpg', image_content)])
+headers = {"Content-Type": content_type, 'content-length': str(len(body))}
+# request = HTTPRequest(url, "POST", headers=headers, body=body, validate_cert=False)
+post_request = tornado.httpclient.HTTPRequest('http://192.168.1.156:8080/person_detection', method='POST', headers=headers, body=body, validate_cert=False)
 # http_client.fetch("http://www.google.com/", handle_response)
 
-interval = 2
+interval = 6000
+interval_fast = 2
+fast_num = 20
 def next_post():
     global interval
     global request_count
     request_count +=1
     print request_count, 'request'
     http_client.fetch(post_request, handle_response)
-    if request_count<200:
-        io_loop.add_timeout(datetime.timedelta(milliseconds=interval), next_post)
+    if request_count%fast_num == 0:
+        next_interval = interval
+    else:
+        next_interval = interval_fast
+    if request_count<2000:
+        io_loop.add_timeout(datetime.timedelta(milliseconds=next_interval), next_post)
 
-# io_loop = tornado.ioloop.IOLoop.current()
-# io_loop.add_timeout(datetime.timedelta(milliseconds=interval), next_post)
-# io_loop.start()
+io_loop = tornado.ioloop.IOLoop.current()
+io_loop.add_timeout(datetime.timedelta(milliseconds=2), next_post)
+io_loop.start()
 
 def print_content(response):
     print response.content
-with open('/home/srzn-office/helmet.jpg', 'rb') as f:
+with open('/home/mythxcq/helmet.jpg', 'rb') as f:
     image_content = f.read()
 
 files = {'image': image_content}
-items = [grequests.post("http://localhost:8080/person_detection", files=files)]
+items = [grequests.post("http://192.168.1.156:8080/person_detection", files=files)]
 items = []
 for i in range(100):
     files = {'image': open('/home/srzn-office/helmet.jpg', 'rb')}
